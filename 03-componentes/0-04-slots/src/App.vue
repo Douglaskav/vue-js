aa2228f5b2884937ca90255043f1a786c71cb43b
Douglas Nicolas <dnmsmpvp@gmail.com>
2020-05-27 09:30:00 -0300

    slots

---
+++ 03-componentes/0-04-slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ModalPrincipal>
+    <template #header="{ logo }">Esse é o header {{ logo }}</template>
+    <template #default="{ user }">
+      <h1>{{user}}</h1>
+      <p>Esse é o modal.</p>
+      <button>Comprar</button>
+    </template>
+    <template #footer>
+      <p>Esse é o footer</p>
+    </template>
+  </ModalPrincipal>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal";
+
+export default {
+  name: "app",
+  components: {
+    ModalPrincipal
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 03-componentes/0-04-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal">
+    <slot name="header" :logo="logo"></slot>
+    <slot :user="user.name"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalPrincipal",
+  data() {
+    return {
+      logo: "Techno",
+      user: {
+        name: "Douglas"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
