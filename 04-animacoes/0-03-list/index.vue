aeb1cdce848f620aa83e6b7cd99c54ec3f23337e
Douglas Nicolas <dnmsmpvp@gmail.com>
2020-05-28 23:11:50 -0300

    List

---
+++ 04-animacoes/0-03-list/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <transition-group tag="ul" v-if="carrinho.length">
+      <li v-for="(produto, index) in carrinho" :key="produto.nome">
+        <p>{{produto.nome}}</p>
+        <p>{{produto.preco}}</p>
+        <button @click="remover(index)">Remover</button>
+      </li>
+    </transition-group>
+    <transition appear v-else>
+      <p>Acabou os produtos</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      carrinho: [
+        {
+          nome: "Notebook",
+          preco: 2399
+        },
+        {
+          nome: "Smarthphone",
+          preco: 3999
+        }
+      ]
+    };
+  },
+
+  methods: {
+    remover(index) {
+      this.carrinho.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+</style>
---
+++ 04-animacoes/0-03-list/move.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <button @click="filterAZ">A - Z</button>
+    <button @click="filterZA">Z - A</button>
+    <transition-group tag="ul">
+      <li v-for="filme in filmes" :key="filme">{{filme}}</li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filmes: [
+        "Senhor Dos Aneis",
+        "As cronicas de Narnia",
+        "Coração Valente",
+        "Harry Potter"
+      ]
+    };
+  },
+
+  methods: {
+    filterAZ() {
+      this.filmes.sort();
+    },
+
+    filterZA() {
+      this.filmes.sort().reverse();
+    }
+  }
+};
+</script>
+
+<style>
+.v-move {
+  transition: transform 0.3s;
+}
+
+li {
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  background: #fff;
+  padding: 20px;
+  max-width: 200px;
+  margin-bottom: 10px;
+}
+</style>
