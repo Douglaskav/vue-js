921fe56175eaabe040c37f4510464538d913ce1d
Douglas Nicolas <dnmsmpvp@gmail.com>
2020-05-28 22:43:05 -0300

    components

---
+++ 04-animacoes/0-02-components/home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <li @click="componenteAtivo = 'PaginaSobre'">Sóbre</li>
+      <li @click="componenteAtivo = 'PaginaServicos'">Serviços</li>
+    </ul>
+    <transition mode="out-in">
+      <component :is="componenteAtivo"></component>
+    </transition>
+  </div>
+</template>
+
+<script>
+import PaginaSobre from "./PaginaSobre";
+import PaginaServicos from "./PaginaServicos";
+
+export default {
+  data() {
+    return {
+      componenteAtivo: "PaginaSobre"
+    };
+  },
+
+  components: {
+    PaginaSobre,
+    PaginaServicos
+  }
+};
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translate3d(0, -20px, 0);
+}
+</style>
